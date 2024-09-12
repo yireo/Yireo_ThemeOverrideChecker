b115691c8e121b6c9a4d2a81450c7636fc2519e5
Jisse Reitsma <jisse@yireo.com>
2024-09-12 14:37:46 +0200

    Add missing

---
+++ Differ/DifferGenerator.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Yireo\ThemeOverrideChecker\Differ;
+
+use SebastianBergmann\Diff\Differ;
+use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
+
+class DifferGenerator
+{
+    /**
+     * @param array $options
+     * @return Differ
+     */
+    public function create(array $options = []): Differ
+    {
+        $options = array_merge([
+            'collapseRanges' => true,
+            'commonLineThreshold' => 6,
+            'contextLines' => 0,
+            'fromFileDate' => null,
+            'toFileDate' => null,
+        ], $options);
+
+        $builder = new StrictUnifiedDiffOutputBuilder($options);
+        return new Differ($builder);
+    }
+}
